ea8897e0a93489c5872a2863fbd4002e9c6e5298
Maxim Silkov <maxim.silkou.2002@gmail.com>
2021-09-27 15:25:27 +0300

    add Dictionary<char, int> FIndOccuranses(string str)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace TILAb2
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dict = Model.FindOccuranses("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        }
     }
 }
---
+++ Model.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace TILAb2
+{
+    class Model
+    {
+        public static Dictionary<char, int> FindOccuranses(string str)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            str = str.ToUpper();
+            foreach (var elem in str)
+            {
+                if (char.IsLetter(elem))
+                {
+                    if (!dict.ContainsKey(elem))
+                    {
+                        dict.Add(elem, 1);
+                    }
+                    else
+                    {
+                        dict[elem]++;
+                    }
+                }
+            }
+
+            return dict;
+        }
+    }
+}
